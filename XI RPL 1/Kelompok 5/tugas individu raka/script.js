9e6f91254078df00ca82ab3958ca0d1f15d9b313
Kaa969 <sillent99999@gmail.com>
2025-08-29 13:46:36 +0700

    Tugas individu raka

---
+++ XI RPL 1/Kelompok 5/tugas individu raka/script.js
@@ -0,0 +1,9 @@
+function toggleExplanation() {
+  const explanation = document.getElementById("extra-explanation");
+  if (explanation.style.display === "none") {
+    explanation.style.display = "block";
+  } else {
+    explanation.style.display = "none";
+  }
+}
+
